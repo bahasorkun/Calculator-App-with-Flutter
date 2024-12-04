0d18cb2334df0e314ee4b0e7425763a4e9b38c84
Baha Sorkun <114817265+bahasorkun@users.noreply.github.com>
2024-12-04 20:39:59 +0300

    feat : "color.dart" , update main.dart

--- calculator_app/lib/main.dart
+++ calculator_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:calculator_app/constants/color.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -9,12 +10,31 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: const AppBarTheme(backgroundColor: darkGrey),
+          scaffoldBackgroundColor: Colors.black),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          title: const Text(
+            'Calculator App',
+            style: TextStyle(color: Colors.white),
+          ),
+          centerTitle: true,
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body:Column(
+          children: [
+            Container(
+              height: 100,
+              margin: EdgeInsets.symmetric(horizontal: 45,vertical: 50),
+              color: darkGrey2,
+            ),
+            Container(
+              alignment: Alignment.bottomCenter,
+              color: Colors.green,
+              
+            )
+          ],
         ),
       ),
     );
